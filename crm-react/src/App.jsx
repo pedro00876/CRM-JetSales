14ee3955548b9b8796d97b1321efaec9adcd777f
pedro00876 <pcchagas00@gmail.com>
2024-09-18 13:44:16 -0300

    Added API

--- crm-react/src/App.jsx
+++ crm-react/src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        "https://crm.rdstation.com/api/v1/contacts?token=66e5f14d9803c30027dc3f70"
+      );
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Olá</h1>
-      <h2>opa</h2>
+      <h1>Lista de Contatos</h1>
+      {products.map((list) => (
+        <li key={list.id}>
+          {list.total} - {list.contacts}
+        </li>
+      ))}
     </div>
   );
 }
